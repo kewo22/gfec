0440c4d374c5bfad06a097fb7dfc1d072547d8d7
KewinFernando <kewinf271@gmail.com>
2023-04-18 00:01:46 +0530

    rem finally

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,12 @@ import GetInTouch from "./components/get-in-touch";
 import Testimonial from "./components/testimonial";
 
 async function getGetInTouchData() {
-  debugger;
   try {
     const url = `https://uat.d2ergyqxpebfoy.amplifyapp.com/api/getintouch`;
     const res = await fetch(url);
     return res.json();
   } catch (error) {
-    const s = { data: "swqd" };
-    return s;
-  } finally {
-    const s = { data: "swqd" };
-    return s;
+    return { data: [] };
   }
 }
 
@@ -32,7 +27,7 @@ export default async function Home() {
     <main className="">
       <h1>ssss - {JSON.stringify(artistData || null)}</h1>
       <h1>ooo - {artistData?.data[0]?.email || "NO"}</h1>
-      <h1>---TEST     ----</h1>
+      <h1>---TEST ----</h1>
       <Hero />
       <About />
       <OurServices />
