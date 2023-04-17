9a135253c7d27b4c3a07b85792984004bf4a5f7c
KewinFernando <kewinf271@gmail.com>
2023-04-17 22:32:01 +0530

    wow api change

--- pages/api/wow.ts
+++ pages/api/wow.ts
@@ -1,35 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import clientPromise from "@/app/libs/MongoConnect";
 import type { NextApiRequest, NextApiResponse } from "next";
-
-import Cors from "cors";
-// import initMiddleware from "../../lib/init-middleware";
 
 type ApiResponse<T> = {
   data?: T;
   message: string;
 };
 
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   res.status(200).json({ name: "John Doe" });
-// }
-
 const get = async (
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse<any>>
 ) => {
-  try {
-    return res
-      .status(200)
-      .json({ data: "ddddddddddddddddddddd", message: "Success" });
-    // return data;
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send({ message: "Server error!" });
-  }
+  return res
+    .status(200)
+    .json({ data: "ddddddddddddddddddddd", message: "Success" });
 };
 
 const post = async (
@@ -43,40 +26,6 @@ const post = async (
   return res.status(200).json({ message: "Success" });
 };
 
-function initMiddleware(middleware: any) {
-  return (req: any, res: any) =>
-    new Promise((resolve, reject) => {
-      middleware(req, res, (result: any) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-        return resolve(result);
-      });
-    });
-}
-
-// Initialize the cors middleware
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ["POST", "OPTIONS"],
-    allowedHeaders: [
-      "X-CSRF-Token",
-      "X-Requested-With",
-      "Accept",
-      "Accept-Version",
-      "Content-Length",
-      "Content-MD5",
-      "Content-Type",
-      "Date",
-      "X-Api-Version",
-    ],
-    origin: "http://localhost:3000/",
-    credentials: false,
-  })
-);
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse<any>>
@@ -89,7 +38,6 @@ export default async function handler(
         break;
 
       case "POST":
-        await cors(req, res);
         //some code...
         post(req, res);
         // res.status(201).json({ message: "POST" });
