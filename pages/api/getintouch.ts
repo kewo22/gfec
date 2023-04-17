e48990b54b5e5b663296eeacbada356e6ee6172a
KewinFernando <kewinf271@gmail.com>
2023-04-17 23:41:54 +0530

    get in touch api fix

--- pages/api/getintouch.ts
+++ pages/api/getintouch.ts
@@ -3,6 +3,7 @@ import clientPromise from "@/app/libs/MongoConnect";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import Cors from "cors";
+import { MongoClient } from "mongodb";
 // import initMiddleware from "../../lib/init-middleware";
 
 type ApiResponse<T> = {
@@ -21,28 +22,44 @@ const get = async (
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse<any>>
 ) => {
+
   try {
-    const client = await clientPromise;
+    const uri =
+      "mongodb+srv://kewo22:pYa4sy0FylRbOB6r@gfec.lwodaum.mongodb.net/gfec?retryWrites=true&w=majority";
+    const client = await new MongoClient(uri.trim()).connect();
+    // const client = await clientPromise;
     const db = await client.db("gfec");
     const collection = db.collection("getInTouch");
-
-    // const collections = GetInTouch.find().exec();
-    // await collection.insertOne({ firstName: "Kewin" });
-    // const data = await GetInTouch.find<GetInTouchType>({});
-
     const data = await collection.find({}).toArray();
 
-    // const data2 = await collection.find<GetInTouchDocument>({});
-    //   .map((obj) => obj.toObject());
-
-    // return NextApiResponse.json({ isConnected: "true", data });
-    return res.status(200).json({ data, message: "Success" });
-    // return data;
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send({ message: "Server error!" });
-    // return NextApiResponse.json({ isConnected: "f" });
+    return res.status(200).json({ data: data, message: "Success - 1" });
+  } catch (error) {
+    return res.status(500).json({ data: "catch err", message: "Success - 2" });
   }
+
+
+  // try {
+  //   const client = await clientPromise;
+  //   const db = await client.db("gfec");
+  //   const collection = db.collection("getInTouch");
+
+  //   // const collections = GetInTouch.find().exec();
+  //   // await collection.insertOne({ firstName: "Kewin" });
+  //   // const data = await GetInTouch.find<GetInTouchType>({});
+
+  //   const data = await collection.find({}).toArray();
+
+  //   // const data2 = await collection.find<GetInTouchDocument>({});
+  //   //   .map((obj) => obj.toObject());
+
+  //   // return NextApiResponse.json({ isConnected: "true", data });
+  //   return res.status(200).json({ data, message: "Success" });
+  //   // return data;
+  // } catch (e) {
+  //   console.error(e);
+  //   return res.status(500).send({ message: "Server error!" });
+  //   // return NextApiResponse.json({ isConnected: "f" });
+  // }
 };
 
 const post = async (
