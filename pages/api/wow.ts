d8c2f7bf6b5d0d50f201bf47c4093dc084b75d60
KewinFernando <kewinf271@gmail.com>
2023-04-17 23:05:49 +0530

    wow ifx

--- pages/api/wow.ts
+++ pages/api/wow.ts
@@ -5,9 +5,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const client = await clientPromise;
-  // const db = await client.db("gfec");
-  // const collection = db.collection("getInTouch");
+  try {
+    const client = await clientPromise;
+    // const db = await client.db("gfec");
+    // const collection = db.collection("getInTouch");
 
-  return res.status(200).json({ data: client, message: "Success" });
+    return res.status(200).json({ data: client, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ data: "catch err", message: "Success" });
+  } finally {
+    return res.status(500).json({ data: "finally err", message: "Success" });
+  }
 }
