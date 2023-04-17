2f4bcac02edcf6ad2962ff04c80a67a921b41fb3
KewinFernando <kewinf271@gmail.com>
2023-04-17 23:21:03 +0530

    code change

--- pages/api/wow.ts
+++ pages/api/wow.ts
@@ -1,4 +1,5 @@
 import clientPromise from "@/app/libs/MongoConnect";
+import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -6,11 +7,14 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   try {
+    const uri =
+      "mongodb+srv://kewo22:pYa4sy0FylRbOB6r@gfec.lwodaum.mongodb.net/gfec?retryWrites=true&w=majority";
+    const client = await new MongoClient(uri.trim()).connect();
     // const client = await clientPromise;
     // const db = await client.db("gfec");
     // const collection = db.collection("getInTouch");
 
-    return res.status(200).json({ data: 'client', message: "Success" });
+    return res.status(200).json({ data: client, message: "Success" });
   } catch (error) {
     return res.status(500).json({ data: "catch err", message: "Success" });
   } finally {
