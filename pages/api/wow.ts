f58bfb16176bd84ebfe81ce7f7bac2795a3b4ca6
KewinFernando <kewinf271@gmail.com>
2023-04-17 20:20:38 +0530

    wow updated

--- pages/api/wow.ts
+++ pages/api/wow.ts
@@ -1,8 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "@/app/libs/MongoConnect";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const get = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+import Cors from "cors";
+// import initMiddleware from "../../lib/init-middleware";
+
+type ApiResponse<T> = {
+  data?: T;
+  message: string;
+};
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: "John Doe" });
+// }
+
+const get = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<any>>
+) => {
   try {
-    return res.status(200).json({ data: "data", message: "Success" });
+    const client = await clientPromise;
+    const db = await client.db("gfec");
+    const collection = db.collection("getInTouch");
+
+    // const collections = GetInTouch.find().exec();
+    // await collection.insertOne({ firstName: "Kewin" });
+    // const data = await GetInTouch.find<GetInTouchType>({});
+
+    const data = await collection.find({}).toArray();
+
+    // const data2 = await collection.find<GetInTouchDocument>({});
+    //   .map((obj) => obj.toObject());
+
+    // return NextApiResponse.json({ isConnected: "true", data });
+    return res.status(200).json({ data, message: "Success" });
+    // return data;
   } catch (e) {
     console.error(e);
     return res.status(500).send({ message: "Server error!" });
@@ -10,19 +45,67 @@ const get = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   }
 };
 
+const post = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<any>>
+) => {
+  const client = await clientPromise;
+  const db = await client.db("gfec");
+  const collection = db.collection("getInTouch");
+  await collection.insertOne(req.body);
+  return res.status(200).json({ message: "Success" });
+};
+
+function initMiddleware(middleware: any) {
+  return (req: any, res: any) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["POST", "OPTIONS"],
+    allowedHeaders: [
+      "X-CSRF-Token",
+      "X-Requested-With",
+      "Accept",
+      "Accept-Version",
+      "Content-Length",
+      "Content-MD5",
+      "Content-Type",
+      "Date",
+      "X-Api-Version",
+    ],
+    origin: "http://localhost:3000/",
+    credentials: false,
+  })
+);
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<ApiResponse<any>>
 ) {
   try {
     switch (req.method) {
       case "GET":
+        //some code...
         get(req, res);
         break;
 
       case "POST":
-        // post(req, res);
-        res.status(201).json({ message: "POST" });
+        await cors(req, res);
+        //some code...
+        post(req, res);
+        // res.status(201).json({ message: "POST" });
         break;
 
       case "PATCH":
@@ -34,7 +117,14 @@ export default async function handler(
         res.status(405).end(`${req.method} Not Allowed`);
         break;
     }
+
+    // if (!allowedMethods.includes(req.method!) || req.method == 'OPTIONS') {
+    //   return res.status(405).send({ message: 'Method not allowed.' });
+    // }
   } catch (error) {
+    // Catch and log errors - return a 500 with a message
+    console.error(error);
+    // Sentry.captureException(error);
     res.status(500).send({ message: "Server Error" });
   }
 }
