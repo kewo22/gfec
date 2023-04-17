a1c4dbac5e401aea5498891f254d30cfaba169f5
KewinFernando <kewinf271@gmail.com>
2023-04-17 22:44:03 +0530

    wow fix

--- pages/api/wow.ts
+++ pages/api/wow.ts
@@ -5,6 +5,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  const client = await clientPromise;
+  const db = await client.db("gfec");
+  const collection = db.collection("getInTouch");
+
   return res
     .status(200)
     .json({ data: "ddddddddddddddddddddd", message: "Success" });
