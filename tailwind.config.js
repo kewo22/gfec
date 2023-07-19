73dba6daf866609d48b9a08accc43facf8ae03db
Kewin Fernando <kewinf271@gmail.com>
2023-07-19 18:10:00 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
+          "50%": { opacity: 0.5 },
           "100%": { opacity: 1 },
         },
       },
