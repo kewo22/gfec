baed57135313b54a6f57498f9da1497ef8c027c5
kewin@kazix.io <kewinf271@gmail.com>
2023-10-02 03:21:04 +0530

    test 3

--- src/app/(site)/_components/navigation.tsx
+++ src/app/(site)/_components/navigation.tsx
@@ -125,10 +125,166 @@ export default function Navigation() {
 
       <Slider {...settings}>
         <div className="bg-home-banner-1 bg-center bg-cover w-full h-full banner-img">
-          <Items />
+          <>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-[90%] max-w-[1000px] flex flex-col gap-10">
+              <Image
+                src="/GFEC-Trans.png"
+                alt="Next.js Logo"
+                width={210}
+                height={160}
+                priority
+                className="!hidden lg:!block mx-auto"
+              />
+              <Image
+                src="/GFEC-Trans.png"
+                alt="Next.js Logo"
+                width={170}
+                height={140}
+                priority
+                className="!block lg:!hidden mx-auto"
+              />
+
+              <NavLinks />
+
+              <div className="relative mx-5 lg:mx-0 flex-grow">
+                <Image
+                  src="/arrow-shape.png"
+                  alt="Next.js Logo"
+                  width={222}
+                  height={92}
+                  priority
+                  className="!hidden lg:!block absolute left-[38%] top-[-5%] animate-bounce"
+                />
+
+                <Typography
+                  variant="xl"
+                  className="text-white !leading-[3.5rem] lg:!leading-[5.5rem]"
+                >
+                  Considering <br /> Studying Abroad ?
+                </Typography>
+
+                <div className="flex flex-col lg:flex-row items-start justify-start gap-5 my-10">
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Free Consultation
+                    </Typography>
+                  </div>
+
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Relocate in 45 Days
+                    </Typography>
+                  </div>
+
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Relocate in 45 Days
+                    </Typography>
+                  </div>
+                </div>
+
+                <NavActions />
+              </div>
+
+              <NavSocial wrapperClass="flex flex-row gap-8 items-center justify-start mx-5 lg:mx-0" />
+            </div>
+          </>
         </div>
         <div className="bg-home-banner-2 bg-center bg-cover w-full h-full banner-img">
-          <Items />
+          <>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-[90%] max-w-[1000px] flex flex-col gap-10">
+              <Image
+                src="/GFEC-Trans.png"
+                alt="Next.js Logo"
+                width={210}
+                height={160}
+                priority
+                className="!hidden lg:!block mx-auto"
+              />
+              <Image
+                src="/GFEC-Trans.png"
+                alt="Next.js Logo"
+                width={170}
+                height={140}
+                priority
+                className="!block lg:!hidden mx-auto"
+              />
+
+              <NavLinks />
+
+              <div className="relative mx-5 lg:mx-0 flex-grow">
+                <Image
+                  src="/arrow-shape.png"
+                  alt="Next.js Logo"
+                  width={222}
+                  height={92}
+                  priority
+                  className="!hidden lg:!block absolute left-[38%] top-[-5%] animate-bounce"
+                />
+
+                <Typography
+                  variant="xl"
+                  className="text-white !leading-[3.5rem] lg:!leading-[5.5rem]"
+                >
+                  Considering <br /> Studying Abroad ?
+                </Typography>
+
+                <div className="flex flex-col lg:flex-row items-start justify-start gap-5 my-10">
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Free Consultation
+                    </Typography>
+                  </div>
+
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Relocate in 45 Days
+                    </Typography>
+                  </div>
+
+                  <div className="flex flex-row gap-2 items-center justify-center">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      size="lg"
+                      className="text-secondary"
+                    />
+                    <Typography variant="h5" className="text-white font-normal">
+                      Relocate in 45 Days
+                    </Typography>
+                  </div>
+                </div>
+
+                <NavActions />
+              </div>
+
+              <NavSocial wrapperClass="flex flex-row gap-8 items-center justify-start mx-5 lg:mx-0" />
+            </div>
+          </>
         </div>
       </Slider>
 
