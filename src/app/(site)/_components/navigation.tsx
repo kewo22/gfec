7b76cf2cbf3eac98f31705f744580f83cd1d50a6
kewin@kazix.io <kewinf271@gmail.com>
2023-10-02 03:22:48 +0530

    test 5

--- src/app/(site)/_components/navigation.tsx
+++ src/app/(site)/_components/navigation.tsx
@@ -33,89 +33,90 @@ export default function Navigation() {
     className: "nav-slider",
   };
 
-  const Items = () => {
-    return (
-      <>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-[90%] max-w-[1000px] flex flex-col gap-10">
-          <Image
-            src="/GFEC-Trans.png"
-            alt="Next.js Logo"
-            width={210}
-            height={160}
-            priority
-            className="!hidden lg:!block mx-auto"
-          />
-          <Image
-            src="/GFEC-Trans.png"
-            alt="Next.js Logo"
-            width={170}
-            height={140}
-            priority
-            className="!block lg:!hidden mx-auto"
-          />
 
-          <NavLinks />
+  // const Items = () => {
+  //   return (
+  //     <>
+  //       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-[90%] max-w-[1000px] flex flex-col gap-10">
+  //         <Image
+  //           src="/GFEC-Trans.png"
+  //           alt="Next.js Logo"
+  //           width={210}
+  //           height={160}
+  //           priority
+  //           className="!hidden lg:!block mx-auto"
+  //         />
+  //         <Image
+  //           src="/GFEC-Trans.png"
+  //           alt="Next.js Logo"
+  //           width={170}
+  //           height={140}
+  //           priority
+  //           className="!block lg:!hidden mx-auto"
+  //         />
 
-          <div className="relative mx-5 lg:mx-0 flex-grow">
-            <Image
-              src="/arrow-shape.png"
-              alt="Next.js Logo"
-              width={222}
-              height={92}
-              priority
-              className="!hidden lg:!block absolute left-[38%] top-[-5%] animate-bounce"
-            />
+  //         <NavLinks />
 
-            <Typography
-              variant="xl"
-              className="text-white !leading-[3.5rem] lg:!leading-[5.5rem]"
-            >
-              Considering <br /> Studying Abroad ?
-            </Typography>
+  //         <div className="relative mx-5 lg:mx-0 flex-grow">
+  //           <Image
+  //             src="/arrow-shape.png"
+  //             alt="Next.js Logo"
+  //             width={222}
+  //             height={92}
+  //             priority
+  //             className="!hidden lg:!block absolute left-[38%] top-[-5%] animate-bounce"
+  //           />
 
-            <div className="flex flex-col lg:flex-row items-start justify-start gap-5 my-10">
-              <div className="flex flex-row gap-2 items-center justify-center">
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  size="lg"
-                  className="text-secondary"
-                />
-                <Typography variant="h5" className="text-white font-normal">
-                  Free Consultation
-                </Typography>
-              </div>
+  //           <Typography
+  //             variant="xl"
+  //             className="text-white !leading-[3.5rem] lg:!leading-[5.5rem]"
+  //           >
+  //             Considering <br /> Studying Abroad ?
+  //           </Typography>
 
-              <div className="flex flex-row gap-2 items-center justify-center">
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  size="lg"
-                  className="text-secondary"
-                />
-                <Typography variant="h5" className="text-white font-normal">
-                  Relocate in 45 Days
-                </Typography>
-              </div>
+  //           <div className="flex flex-col lg:flex-row items-start justify-start gap-5 my-10">
+  //             <div className="flex flex-row gap-2 items-center justify-center">
+  //               <FontAwesomeIcon
+  //                 icon={faCheck}
+  //                 size="lg"
+  //                 className="text-secondary"
+  //               />
+  //               <Typography variant="h5" className="text-white font-normal">
+  //                 Free Consultation
+  //               </Typography>
+  //             </div>
 
-              <div className="flex flex-row gap-2 items-center justify-center">
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  size="lg"
-                  className="text-secondary"
-                />
-                <Typography variant="h5" className="text-white font-normal">
-                  Relocate in 45 Days
-                </Typography>
-              </div>
-            </div>
+  //             <div className="flex flex-row gap-2 items-center justify-center">
+  //               <FontAwesomeIcon
+  //                 icon={faCheck}
+  //                 size="lg"
+  //                 className="text-secondary"
+  //               />
+  //               <Typography variant="h5" className="text-white font-normal">
+  //                 Relocate in 45 Days
+  //               </Typography>
+  //             </div>
 
-            <NavActions />
-          </div>
+  //             <div className="flex flex-row gap-2 items-center justify-center">
+  //               <FontAwesomeIcon
+  //                 icon={faCheck}
+  //                 size="lg"
+  //                 className="text-secondary"
+  //               />
+  //               <Typography variant="h5" className="text-white font-normal">
+  //                 Relocate in 45 Days
+  //               </Typography>
+  //             </div>
+  //           </div>
 
-          <NavSocial wrapperClass="flex flex-row gap-8 items-center justify-start mx-5 lg:mx-0" />
-        </div>
-      </>
-    );
-  };
+  //           <NavActions />
+  //         </div>
+
+  //         <NavSocial wrapperClass="flex flex-row gap-8 items-center justify-start mx-5 lg:mx-0" />
+  //       </div>
+  //     </>
+  //   );
+  // };
 
   return (
     <nav className={`h-[900px] flex flex-col items-center relative`}>
