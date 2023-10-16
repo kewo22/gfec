25d94722baadf3a80965e763b258bac786f33782
kewin@kazix.io <kewinf271@gmail.com>
2023-10-17 03:47:28 +0530

    read more added

---
+++ src/app/_components/ui/modal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  faTimesCircle,
+  faTimesSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect } from "react";
+
+type ModalProps = {
+  isOpen: boolean;
+  children: React.ReactNode;
+  onClose: () => void;
+};
+
+export default function Modal(props: ModalProps) {
+  const { children, isOpen = false, onClose } = props;
+
+  useEffect(() => {
+    if (isOpen) {
+      const bodyEl = document.querySelector("body");
+      const firstChild = bodyEl?.firstChild;
+      const modalEl = document.querySelector("#modal")!;
+      bodyEl!.insertBefore(modalEl, firstChild!);
+      bodyEl?.classList.add("overflow-hidden");
+      setTimeout(() => {
+        modalEl.classList.add("scale-0");
+        modalEl.classList.add("scale-100");
+      }, 100);
+    } else {
+      //
+    }
+  }, [isOpen]);
+
+  const onCloseClick = () => {
+    const bodyEl = document.querySelector("body");
+    bodyEl?.classList.remove("overflow-hidden");
+    const modalEl = document.querySelector("#modal")!;
+    modalEl.classList.add("scale-0");
+    modalEl.classList.remove("scale-100");
+    onClose();
+  };
+
+  return (
+    <section
+      id="modal"
+      className="fixed z-50 bg-slate-950/50 scale-0 transition-all duration-500 ease-in-out top-0 left-0 bottom-0 right-0 grid place-items-center h-screen w-screen"
+    >
+      <div className="relative max-h-[90%] h-auto w-[90%] max-w-3xl bg-white rounded-lg overflow-y-auto overflow-x-hidden p-5">
+        <FontAwesomeIcon
+          icon={faTimesCircle}
+          size="2x"
+          className="text-slate-500 absolute right-5 top-5"
+          onClick={onCloseClick}
+        />
+        <div className="mt-5">{children}</div>
+      </div>
+    </section>
+  );
+}
