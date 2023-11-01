5a8bcfbef3271aa1dfc0e86fcb061d1e94ad222e
kewin@kazix.io <kewinf271@gmail.com>
2023-11-02 03:25:18 +0530

    fixes

--- src/app/_components/ui/modal.tsx
+++ src/app/_components/ui/modal.tsx
@@ -14,7 +14,6 @@ type ModalProps = {
 
 export default function Modal(props: ModalProps) {
   const { children, isOpen = false, type, onClose } = props;
-  console.log("🚀 ~ file: modal.tsx:19 ~ Modal ~ type:", type, isOpen);
 
   const test = () => {
     onClose();
@@ -30,37 +29,25 @@ export default function Modal(props: ModalProps) {
           "fixed z-50 top-0 left-0 h-full w-full overflow-hidden bg-slate-950/50 flex items-center justify-center",
       },
       createElement(
-        "button",
-        {
-          className:
-            "absolute right-5 top-5 cursor-pointer hover:text-secondary transition-all duration-500 ease-in-out",
-          onClick: () => {
-            test();
-          },
-        },
-        <FontAwesomeIcon icon={faTimesCircle} size="2x" />
-      ),
-      createElement(
         "div",
         {
           className: "bg-white mx-5 p-5",
-          onClick: () => {
-            test();
-          },
         },
-        children
+        <>
+          <div className="flex justify-end">
+            <FontAwesomeIcon icon={faTimesCircle} size="2x" onClick={test} />
+          </div>
+          {children}
+        </>
       )
     );
 
     return u;
   } else {
     setTimeout(() => {
-      console.log(document);
-    }, 1000);
-    // if (document) {
-    //   const bodyEl = document.querySelector("body");
-    //   bodyEl?.classList.remove("overflow-hidden");
-    // }
+      const bodyEl = document.querySelector("body");
+      bodyEl?.classList.remove("overflow-hidden");
+    }, 500);
     return <></>;
   }
 }
