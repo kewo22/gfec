d609694fd6f34abe6b1b50bf61ce267e06abc83e
Kewin22 <kewinf271@gmail.com>
2023-11-04 12:06:51 +0400

    test prod commit

--- src/app/(site)/_constants/nav-items.constants.ts
+++ src/app/(site)/_constants/nav-items.constants.ts
@@ -2,7 +2,7 @@ import { NavItemLink } from "../_types/mobile-nav-item";
 
 export const NavItems: NavItemLink[] = [
   {
-    text: "Home",
+    text: "Home1",
     route: "/",
     class: "",
     isActive: false,
