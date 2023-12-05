98e6a9c42a4291026e6511ffbd1d141669a13f78
kewin@kazix.io <kewinf271@gmail.com>
2023-12-05 12:19:09 +0530

    data table added shad

--- src/app/(dashboard)/application/page.tsx
+++ src/app/(dashboard)/application/page.tsx
@@ -1,5 +1,62 @@
 import React from "react";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/_components/ui/table";
+import {
+  ColumnDef,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { DataGrid } from "./DataGrid";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+];
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+];
+
 export default function Application() {
-  return <div className=" ">Application</div>;
+  return (
+    <div className="max-w-5xl mx-auto">
+      <DataGrid />
+    </div>
+  );
 }
