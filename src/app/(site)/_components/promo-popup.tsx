2f39c95bb546f92788b8ae7c8d9d073b553f4236
kewin-z <kewinf271@gmail.com>
2024-10-08 00:17:48 +0400

    submit data done

--- src/app/(site)/_components/promo-popup.tsx
+++ src/app/(site)/_components/promo-popup.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import Button from "@/app/_components/ui/button";
 
 export default function PromoPopUp() {
+  const router = useRouter();
 
   const [isShow, setIsShow] = useState(false)
 
@@ -15,23 +18,34 @@ export default function PromoPopUp() {
       setIsShow(true)
       document.body.classList.add('!overflow-hidden');
       setTimeout(() => {
-        console.log(document.querySelector("#ad-img"))
         document.querySelector("#ad-img")?.classList.replace('scale-0', 'scale-1')
+        document.querySelector(".ad-btn")?.classList.replace('scale-0', 'scale-1')
       }, 500);
     }, 500);
   }, [])
 
   const onCloseAd = () => {
     document.querySelector("#ad-img")?.classList.replace('scale-1', 'scale-0')
+    document.querySelector(".ad-btn")?.classList.replace('scale-1', 'scale-0')
     setTimeout(() => {
       setIsShow(false)
       document.body.classList.remove('!overflow-hidden')
     }, 500);
   }
 
+  const onRegisterClick = () => {
+    document.querySelector("#ad-img")?.classList.replace('scale-1', 'scale-0')
+    document.querySelector(".ad-btn")?.classList.replace('scale-1', 'scale-0')
+    setTimeout(() => {
+      setIsShow(false)
+      document.body.classList.remove('!overflow-hidden')
+    }, 500);
+    router.push('/open-day-register', { scroll: true })
+  }
+
   if (isShow) {
     return (
-      <div className="fixed top-0 right-0 w-full h-full z-50 flex items-center justify-center bg-[#000000a1]">
+      <div className="fixed top-0 right-0 w-full h-full z-50 flex flex-col items-center justify-center gap-2 bg-[#000000a1]">
         <button className="absolute top-2 right-2 bg-white h-10 w-10 rounded-full p-2">
           <FontAwesomeIcon
             icon={faClose}
@@ -50,6 +64,19 @@ export default function PromoPopUp() {
           className="w-[calc(100%-10%)] md:w-[calc(100%-40%)] lg:w-[calc(100%-55%)] scale-0 transition-all ease-in-out duration-500"
           id="ad-img"
         />
+
+        <Button
+          text="Click to register"
+          size="md"
+          customClass="w-fit bg-white text-secondary scale-0 transition-all ease-in-out duration-500 ad-btn" 
+          onClick={() => {
+            onRegisterClick();
+          }}
+        />
+
+        {/* <button className="bg-white p-5 text-primary rounded" onClick={() => onRegisterClick()}>
+          Click to register
+        </button> */}
       </div>
     );
   }
