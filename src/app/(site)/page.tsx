b6024eb530763be79ae25af4e77f8f4fed5cebd0
kewin-z <kewinf271@gmail.com>
2025-11-12 22:10:41 +0400

    whatsapp added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { SocialIcon } from 'react-social-icons';
+
 import SuccessStoriesText from "./_components/success-stories-text";
 import WhatGfecOffers from "./_components/what-gfec-offers";
 import SummaryCounterGroup from "./_components/summary-counter-group";
@@ -23,6 +27,21 @@ export default function SitePage() {
       </div>
       <SuccessPathNew />
       <SuccessStoriesText />
+
+      {/* <a
+        className="whatsapp-float"
+        href="https://wa.me/15551234567?text=Hello%20there%2C%20I%27d%20like%20help%20with%20..."
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Chat on WhatsApp"
+      > */}
+      {/*   */}
+      {/* onClick={(event) => event.preventDefault()}  */}
+      <div className='fixed z-[999999999] bottom-5 right-5'>
+        <SocialIcon network="whatsapp" url="https://wa.me/+94771789038" target="_blank" />
+      </div>
+      {/* </a> */}
+
     </section>
   );
 }
