0c574d164c4f47af9e776bb2688762f3f6856e49
kewin-z <kewinf271@gmail.com>
2025-11-12 22:21:04 +0400

    kew num added from temp

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -25,7 +25,7 @@ export default function SiteLayout({
       <Analytics />
       <SpeedInsights />
       <div className='fixed z-[999999999] bottom-5 right-5'>
-        <SocialIcon network="whatsapp" url="https://wa.me/+94773889161" target="_blank" />
+        <SocialIcon network="whatsapp" url="https://wa.me/+94771789038" target="_blank" />
       </div>
     </div>
   );
